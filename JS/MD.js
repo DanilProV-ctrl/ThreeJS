f04418dce183fc1092a16370eddb24f245457558
DanilProV-ctrl <ProVDanil2019@yandex.ru>
2020-02-19 18:21:37 +1000

    Update Planet.

--- JS/MD.js
+++ JS/MD.js
@@ -25,17 +25,21 @@ window.onload = function() {
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(60, width / height, 0.1, 5000);
     camera.position.set(0, 0, 1000);
-    var light = new THREE.AmbientLight(0xffffff);
+    var light = new THREE.SpotLight();
+    light.castShadow = true;
+    light.position.set(0, 0, 1000);
     scene.add(light);
     var geometry = new THREE.SphereGeometry(200, 60, 60);
     var loader = new THREE.TextureLoader();
-    var material = new THREE.MeshBasicMaterial({ map: loader.load('https://lh5.googleusercontent.com/-Ochc8HOY8uE/URDsSk9NZ9I/AAAAAAAAA0E/eoV5-EU7Xuk/s1024/earthmap1k.jpg'), });
+    var material = new THREE.MeshLambertMaterial({ map: loader.load('https://lh5.googleusercontent.com/-Ochc8HOY8uE/URDsSk9NZ9I/AAAAAAAAA0E/eoV5-EU7Xuk/s1024/earthmap1k.jpg'), });
 
     /*for (var i = 0; i < geometry.faces.length; i++) {
         geometry.faces[i].color.setRGB(Math.random(), Math.random(), Math.random());
     }*/
 
     var Mesh = new THREE.Mesh(geometry, material);
+    Mesh.castShadow = true;
+    Mesh.reactiveShadow = true;
     scene.add(Mesh);
 
     function loop() {
@@ -46,6 +50,7 @@ window.onload = function() {
         Mesh.position.x += ball.positionX;
         Mesh.position.z += ball.positionZ;
         renderer.render(scene, camera);
+        renderer.mapShadowEnabled = true;
         requestAnimationFrame(function() { loop(); });
     }
     loop();
